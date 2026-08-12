71c81fb46d379355702e5cd84d4abe1918852277
coryparrry <coryhparry@gmail.com>
2026-08-12 07:15:56 +0100

    chore(release): pin editor argument checkpoint

--- packages/codekeeper/test/helpers.mjs
+++ packages/codekeeper/test/helpers.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from "node:url";
 
 export const PACKAGE_ROOT = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
 export const REPOSITORY_ROOT = path.resolve(PACKAGE_ROOT, "../..");
-export const PINNED_COMMIT = "7367bcfe6bfeedf4e44b49792f9cd1b6bb1dbbfc";
+export const PINNED_COMMIT = "30de4f6ce252b9943008cbbd99f7e304250212ea";
 export const HEAD_SHA = "a".repeat(40);
 
 export function result(stdout = "", overrides = {}) {
